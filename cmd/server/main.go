945722dfb91755298675d73fc89af8b944eb4e3b
Ilya Ermakov <i.ermakov@nic.ru>
2025-10-30 00:40:58 +0300

    123

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,21 +10,6 @@ import (
 )
 
 func main() {
-	database.InitDB()
-	repo := task.NewRepository(database.DB)
-	service := task.NewService(repo)
-
-	userClient, conn, err := transportgrpc.NewUserClient("localhost:50051")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	go func() {
-		log.Println("pprof on :6060")
-		if err := http.ListenAndServe(":6060", nil); err != nil {
-			log.Fatalf("pprof failed: %v", err)
-		}
-	}()
 	if err := transportgrpc.RunGRPC(service, userClient); err != nil {
 		log.Fatal(err)
 	}
